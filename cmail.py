f97bf810954c04c1bf856de78889066cf1ef05f3
N-Pujitha <pujithan64@gmail.com>
2023-04-03 12:07:45 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,24 @@
+import smtplib
+from smtplib import SMTP
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('pujithan64@gmail.com ','avvsvgohvhthzaxn')
+    msg=EmailMessage()
+    msg['From']='pujithan64@gamil.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+
+    
+
+
+
+
+
+
+
+        
+    
